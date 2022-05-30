8f55a110814153403fc58f1d9f4678c08b3f62e5
vincent <paintart.lin@gmail.com>
2022-05-30 16:38:57 +0800

    feat(MainPanel.js): can deploy contract using MetaMask

--- frontend/src/components/MainPanel.js
+++ frontend/src/components/MainPanel.js
@@ -1,14 +1,18 @@
 import {useEffect, useState} from 'react';
 import Web3 from 'web3';
 import TransferForm from './TransferForm.js';
-import {MainContract} from '../contracts/MainContract.js';
+import MainContract from '../contracts/MainContract.json';
 import {getWeb3} from '../utils';
 import { Space, Card, Form } from 'antd';
 import AddressTable from './AddressTable.js';
 const MainPanel =
     ({userInfo}) => {
-      const [account, setAccount] = useState(null);
+      const [walletAddress, setWalletAddress] = useState(null);
       const [balance, setBalance] = useState(0.0);
+
+      const [contractAddress, setContractAddress] = useState(null);
+      const [contractBalance, setContractBalance] = useState(0.0);
+
       const [web3, setWeb3] = useState(null);
       const [addressData, setAddressData] = useState([]);
       const [form] = Form.useForm();
@@ -21,7 +25,7 @@ const MainPanel =
         const _web3 = await getWeb3();
         setWeb3(_web3);
         const accounts = await _web3.eth.getAccounts();
-        setAccount(accounts[0]);
+        setWalletAddress(accounts[0]);
         const _balance = await _web3.eth.getBalance(accounts[0]) / 1e18;
         console.log(_balance)
             setBalance(_balance);
@@ -31,11 +35,33 @@ const MainPanel =
             // });
       }
 
-      const deployContract =
-          async () => {
-        const contract = new web3.eth.Contract(MainContract);
-        contract.deploy().send();
+      const deployContract = async () => {
+        const _contract = new web3.eth.Contract(MainContract.abi);
+        _contract.deploy({
+          data: MainContract.bytecode,
+          arguments: [walletAddress]
+        }).send({
+          from: walletAddress
+        }).then(deployedContract => {
+          const _contractAddress = deployedContract.options.address;
+          console.log(`Contract deployed at ${_contractAddress} with ${_contractBalance} Eth`);
+          setContractAddress(_contractAddress);
+        }).catch(err => {
+          console.log('Failed to deploy contact:', err);
+        });
       }
+
+      const fetchContractInfo = async (address) => {
+        const _contractBalance = web3.eth.getBalance(address);
+        setContractBalance(_contractBalance);
+        const _contract = new web3.eth.Contract(MainContract.abi, address);
+        const _addressData = _contract.methods.getBeneficiary.call();
+        console.log(_addressData);
+      }
+
+      useEffect(() => {
+        fetchContractInfo(contract);
+      }, [contract]);
   
       return (
         <Space direction='vertical'>
@@ -49,7 +75,7 @@ const MainPanel =
             }}
           >
             <p>錢包地址:</p>
-            <p>{account}</p>
+            <p>{walletAddress}</p>
             <p>目前錢包餘額: {balance}</p>
           </Card>
           <TransferForm balance={{ wallet: balance, bank: 3 }} form={form}/>
@@ -60,17 +86,13 @@ const MainPanel =
               background: '#ececec'
             }}
           >
+            <button onClick = {deployContract}>部署</button>
             <p>保險錢包餘額: {3}</p>
           </Card>
           </Space>
           <AddressTable addressData={addressData} setAddressData={setAddressData}></AddressTable>
         </Space>
       );
-        // <>{account}<p>目前錢包餘額：{
-        //   balance}</p>
-        //     <p>保險錢包餘額：{}</p>
-        //       <button onClick = {deployContract}>部署</button>
-        // </>);
     }
 
 export default MainPanel;
